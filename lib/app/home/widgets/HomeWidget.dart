bd7f0a54c154b0997cf0fa1de668b700e5ca8cfd
Zoom404 <54240149+Zoom404@users.noreply.github.com>
2020-04-30 10:18:08 +0530

    submit

--- lib/app/home/widgets/HomeWidget.dart
+++ lib/app/home/widgets/HomeWidget.dart
@@ -2,13 +2,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import '../../config/ColorsSet.dart';
-import '../../config/ColorsSet.dart';
-import '../../config/ColorsSet.dart';
-import '../../config/ColorsSet.dart';
-import '../../config/ColorsSet.dart';
-import '../../config/ColorsSet.dart';
-import '../../config/ColorsSet.dart';
-import '../../config/ImagesSet.dart';
 import '../../config/ImagesSet.dart';
 
 class HomeWidget extends StatefulWidget {
@@ -46,14 +39,15 @@ class _HomeWidgetState extends State<HomeWidget> {
                     ),
                     CupertinoButton(
                       padding: EdgeInsets.all(0),
-                      onPressed: (){},
+                      onPressed: () {},
                       child: Container(
                         height: 35,
                         width: 150,
                         alignment: Alignment.center,
                         child: Text(
                           "Check",
-                          style: TextStyle(color: AppColors.white, fontSize: 16),
+                          style:
+                              TextStyle(color: AppColors.white, fontSize: 16),
                         ),
                         decoration: BoxDecoration(
                             borderRadius: BorderRadius.all(Radius.circular(20)),
@@ -94,7 +88,7 @@ class _HomeWidgetState extends State<HomeWidget> {
                 ),
                 CupertinoButton(
                   padding: EdgeInsets.all(0),
-                  onPressed: (){},
+                  onPressed: () {},
                   child: Text(
                     "View all",
                     style: TextStyle(
@@ -109,71 +103,87 @@ class _HomeWidgetState extends State<HomeWidget> {
           height: 300,
           child: ListView.builder(
             shrinkWrap: true,
-              scrollDirection: Axis.horizontal,
-              itemBuilder: (context, index) {
-                return Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Container(
-                    margin: EdgeInsets.all(10),
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Stack(
-                          children: <Widget>[
-
-                            Container(
-                              width: 120,
-                              height: 200,
-                              decoration: BoxDecoration(
-                                image: DecorationImage(
-                                    fit: BoxFit.cover,
-                                    image:AssetImage(ImagesSet.homeProduct+(index+1).toString()+'.png')
-                                ),
-                                borderRadius: BorderRadius.all(Radius.circular(8.0)),
-                                color: Colors.redAccent,
+            scrollDirection: Axis.horizontal,
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  margin: EdgeInsets.all(10),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Stack(
+                        children: <Widget>[
+                          Container(
+                            width: 120,
+                            height: 200,
+                            decoration: BoxDecoration(
+                              image: DecorationImage(
+                                  fit: BoxFit.cover,
+                                  image: AssetImage(ImagesSet.homeProduct +
+                                      (index + 1).toString() +
+                                      '.png')),
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(8.0)),
+                              color: Colors.redAccent,
+                            ),
+                          ),
+                          Container(
+                            alignment: Alignment.center,
+                            child: Padding(
+                              padding: const EdgeInsets.only(
+                                  left: 8.0, right: 8, top: 3, bottom: 3),
+                              child: Text(
+                                "-20%",
+                                style: TextStyle(
+                                    color: AppColors.white,
+                                    fontWeight: FontWeight.w800),
                               ),
                             ),
-
-
-                            Container(
-                              alignment: Alignment.center,
-                              child: Padding(
-                                padding: const EdgeInsets.only(left:8.0, right: 8, top: 3, bottom: 3),
-                                child: Text("-20%", style: TextStyle(color: AppColors.white,
-                                fontWeight: FontWeight.w800),),
-                              ),
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.all(Radius.circular(5)),
-                                color: AppColors.black
-                              ),
-                            )
-                          ],
-                        ),
-                        SizedBox(height: 5,),
-                        Text("T-shirt", style:
-                        TextStyle(fontSize: 16, fontWeight: FontWeight.w200),),
-
-                        Row(
-                          children: <Widget>[
-                            Text("300 ₹", style: TextStyle(
+                            decoration: BoxDecoration(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(5)),
+                                color: AppColors.black),
+                          )
+                        ],
+                      ),
+                      SizedBox(
+                        height: 5,
+                      ),
+                      Text(
+                        "T-shirt",
+                        style: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.w200),
+                      ),
+                      Row(
+                        children: <Widget>[
+                          Text(
+                            "300 ₹",
+                            style: TextStyle(
                               fontSize: 12,
                               decoration: TextDecoration.lineThrough,
-                            ),),
-                            SizedBox(height: 5,),
-                            Text("200 ₹", style: TextStyle(
+                            ),
+                          ),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          Text(
+                            "200 ₹",
+                            style: TextStyle(
                               fontSize: 12,
-                            ),),
-                          ],
-                        ),
-
-                      ],
-                    ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
                   ),
-                );
-              }, itemCount: 3,),
+                ),
+              );
+            },
+            itemCount: 3,
+          ),
         ),
-
       ],
     ));
   }
